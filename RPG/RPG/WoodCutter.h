fc796a70b02001ad3d036fce3501df2d610665ec
elliott <furby_with_a_gun@hotmail.com>
2021-11-30 17:05:13 -0400

    started work on woodcutter building

---
+++ RPG/RPG/WoodCutter.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "Building.h"
+
+#include "RpgGameConstants.h"
+
+class WoodCutter : public Building
+{
+public:
+    //attributes
+
+    //constructors
+    WoodCutter();
+    WoodCutter(SaveObject saveObject);
+    WoodCutter(int newWidth, int newHeight);
+    WoodCutter(std::vector< std::vector<BuildingTile*> > newTileMap);
+    WoodCutter(int entranceDirection);
+
+    //methods
+    int onActionAssignedUnit(RpgUnit* unit) override;
+    void update(RpgTileGridScene* scene) override;
+
+private:
+    //attributed
+    int troopSpawnTick;
+    int troopSpawnRate;
+
+    void init();
+};
+
